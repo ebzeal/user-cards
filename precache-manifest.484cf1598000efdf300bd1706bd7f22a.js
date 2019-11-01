efd193ff251767dc457ea59df1e9f8f8c8577d7f
Olusola Ajayi <ebenezerajayi@gmail.com>
2019-11-01 13:53:07 +0100

    Updates

---
+++ precache-manifest.484cf1598000efdf300bd1706bd7f22a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71838d257159eff08fd65a1d126c172",
+    "url": "/user-cards/index.html"
+  },
+  {
+    "revision": "1b987768e887e980c548",
+    "url": "/user-cards/static/css/main.99c12d9a.chunk.css"
+  },
+  {
+    "revision": "a475419a94347128bef4",
+    "url": "/user-cards/static/js/2.e5938270.chunk.js"
+  },
+  {
+    "revision": "1b987768e887e980c548",
+    "url": "/user-cards/static/js/main.252d12c9.chunk.js"
+  },
+  {
+    "revision": "19b4855c707ddc201aff",
+    "url": "/user-cards/static/js/runtime-main.74882122.js"
+  }
+]);
